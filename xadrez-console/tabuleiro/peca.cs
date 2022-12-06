13d225487d7daf3fe0345d4892a62a3ff14c146f
EduardoAzSouza <EduardoAzSouza@users.noreply.github.com>
2022-12-06 17:13:21 -0300

    implememntando restrições de movimentos

--- xadrez-console/tabuleiro/peca.cs
+++ xadrez-console/tabuleiro/peca.cs
@@ -1,20 +1,57 @@
-﻿using tabuleiro;
-
+﻿
 namespace tabuleiro
 {
-    class Peca
+    abstract class Peca
     {
         public Posicao posicao { get; set; }
-        public Cor Cor { get; protected set; }
-        public int qMovimentos { get; protected set; }
+        public Cor cor { get; protected set; }
+        public int qteMovimentos { get; protected set; }
         public Tabuleiro tab { get; protected set; }
 
         public Peca(Tabuleiro tab, Cor cor)
         {
-            this.posicao = null; 
+            this.posicao = null;
             this.tab = tab;
-            this.Cor = cor;
-            this.qMovimentos = 0;
+            this.cor = cor;
+            this.qteMovimentos = 0;
         }
+
+        public void incrementarQteMovimentos()
+        {
+            qteMovimentos++;
+        }
+
+        public void decrementarQteMovimentos()
+        {
+            qteMovimentos--;
+        }
+
+        public bool existeMovimentosPossiveis()
+        {
+            bool[,] mat = movimentosPossiveis();
+            for (int i = 0; i < tab.linhas; i++)
+            {
+                for (int j = 0; j < tab.colunas; j++)
+                {
+                    if (mat[i, j])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool podeMoverPara(Posicao pos)
+        {
+            return movimentosPossiveis()[pos.linha, pos.coluna];
+        }
+
+        public bool movimentoPossivel(Posicao pos)
+        {
+            return movimentosPossiveis()[pos.linha, pos.coluna];
+        }
+
+        public abstract bool[,] movimentosPossiveis();
     }
 }
